79aeab77532731606d916e6c54e08c1c5d47c96b
Monosacarideo <110903472+Monosacarideo@users.noreply.github.com>
2023-12-11 17:22:46 -0300

    Add files via upload

---
+++ Projeto-OO-main/Validacao.java
@@ -0,0 +1,36 @@
+public class Validacao {
+    
+    // Validar usuário
+    public static boolean validarIdadeVoluntario(int idadeVoluntario) {
+        boolean voluntarioValido = false;
+        if (idadeVoluntario >= 18) {
+            voluntarioValido = true;
+
+            if (idadeVoluntario < 18) {
+
+            }
+            return voluntarioValido;
+        }
+        return voluntarioValido;
+    }
+
+    // Validar se é dinheiro a doação
+    public static boolean validarSeForDinheiro(Doacao d) {
+        boolean doarDinheiro = false;
+        if (d.categoria == "Dinheiro") {
+            doarDinheiro = true;
+
+            if (d.categoria != "Dinheiro") {
+
+            }
+            return doarDinheiro;
+        }
+        return doarDinheiro;
+    }
+    
+    // Validar cadastro usuário
+
+    // Validar quantidade doação
+
+    // Validar id doação
+}
